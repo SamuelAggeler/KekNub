87b37ab13a067ff0a777695cea83fabcb0953a7e
Samuel Aggeler <Samuel.aggeler@outlook.com>
2021-06-01 16:43:11 +0200

    inital commit

---
+++ base.py
@@ -0,0 +1,32 @@
+import os
+import discord
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
+
+client = discord.Client()
+
+@client.event
+async def on_read():
+    for guild in client.guilds:
+        if guild.name == GUILD:
+            break
+
+
+    print(
+        f'{client.user} is connected to the following guild:\n'
+        f'{guild.name}(id: {guild.id})'
+    )
+
+    members = '\n - '.join([member.name for member in guild.members])
+    print(f'Guild Members:\n - {members}')
+
+
+@client.event
+async def on_message(message):
+    if 'fabian' in message.content.lower():
+        await message.channel.send('Schnitte! :smirk: ')
+
+client.run(TOKEN)
