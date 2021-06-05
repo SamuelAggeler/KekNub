344dbe973cd0a1bd5ec6460e15de0e6772554e7f
Samuel Aggeler <Samuel.aggeler@outlook.com>
2021-06-05 10:58:50 +0200

    fixed problem with bot not responding to commands

--- base.py
+++ base.py
@@ -1,6 +1,6 @@
 import os
 import discord
-import random
+import random as rand
 
 from discord import utils
 from discord import channel
@@ -8,16 +8,37 @@ from discord.ext import commands
 from discord.abc import GuildChannel
 from dotenv import load_dotenv
 
+
+
+#load token, discord and intents 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
 
 
+
+
+#initalize bot prefix
 bot = commands.Bot(command_prefix='?')
 
-@bot.command(name='create-channel')
+#helper message to see, when the bot is connected
+@bot.event
+async def on_ready():
+    guild = discord.utils.get(bot.guilds, name=GUILD)
+    print(
+        f'{bot.user} is connected to the following guild:\n'
+        f'{guild.name}(id: {guild.id})'
+    )
+ 
+#takes arbitrary amount of arguments and returns a random one
+@bot.command(name = 'random', help='takes input and returns random choice')
+async def random(ctx, *args):
+    await ctx.send(rand.choice(args))
+
+
+@bot.command()
 @commands.has_role(':)')
-async def create_channel(ctx, channel_name='real-python'):
+async def create_channel(ctx, channel_name='testchannel1'):
     guild = ctx.guild
     existing_channel = discord.utils.get(guild.channels, name=channel_name)
     if not existing_channel:
@@ -25,26 +46,13 @@ async def create_channel(ctx, channel_name='real-python'):
         await guild.create_text_channel(channel_name)
 
 
-
-@bot.event
-async def on_message(message):
-    if 'fabian' in message.content.lower():
-        await message.channel.send('Schnitte! :smirk: ')
-
-
-
-@bot.command(name = '99', help='this is a helper command')
-async def test(ctx):
-    testquote = ['test']
-
-    response = random.choice(testquote)
-    await ctx.send(response)
-
-
-
 @bot.event
 async def on_command_error(ctx, error):
     if isinstance(error, commands.errors.CheckFailure):
         await ctx.send('You do not have the correct role for this command.')
+
+
+
+
 
 bot.run(TOKEN)
