7b8de42326b31c372cc89ee6acc65d49dd45febe
Samuel Aggeler <Samuel.aggeler@outlook.com>
2021-06-05 23:42:48 +0200

    added some features for base py

--- base.py
+++ base.py
@@ -1,11 +1,15 @@
 import os
 import discord
 import random as rand
+import datetime
+import asyncio
+import utility
 
 from discord import utils
 from discord import channel
 from discord.ext import commands
 from discord.abc import GuildChannel
+from discord.user import ClientUser
 from dotenv import load_dotenv
 
 
@@ -45,14 +49,55 @@ async def create_channel(ctx,channel_name):
         print(f'Creating a new channel: {channel_name}')
         await guild.create_text_channel(channel_name)
 
-
+#small errorhandling if someone without :) roles tries to create a text channel
 @bot.event
 async def on_command_error(ctx, error):
     if isinstance(error, commands.errors.CheckFailure):
         await ctx.send('You do not have the correct role for this command.')
 
+@bot.command()
+async def reaction(ctx):
+    message = 'this should send a thumb'
+    emoji = '👍'
+    await ctx.send(message + emoji)
+
+@bot.command(help = "start a giveaway takes time in mins and then the prize as \na String")
+async def startgive(ctx, mins : int, *, prize: str):
+    embed = discord.Embed(title = 'Giveaway', description = f"{prize}", color = ctx.author.color)
+
+    end = datetime.datetime.utcnow() + datetime.timedelta(seconds = mins*60)
+
+    embed.add_field(name = "Ends At: ", value = f"{end} UTC")
+    embed.set_footer(text = "Ends {mins} minutes from now!" )
+
+    my_msg = await ctx.send(embed = embed)
+
+    await my_msg.add_reaction("👍")
+
+    await asyncio.sleep(mins*60)
+
+    new_msg = await ctx.channel.fetch_message(my_msg.id)
+
+    user = new_msg.reaction[0].users().flatten()
+    user.pop(user.index(bot.user))
+
+    winner = random.choice(user)
+
+    await ctx.send(f"Congratulation {winner.mention} won {prize}")
 
 
+#testreaction that puts a thumb under a message that contains thumbs
+@bot.listen('on_message')
+async def addingEmote(message):
+    if 'thumbs' in message.content.lower():
+        emoji = '👍'
+        await message.add_reaction(emoji)
+
+#pings user a certain amount of time with a custom message
+@bot.command(help = "takes a user, amount of pings and a message as argument")
+async def ping(ctx, user1 : discord.Member, amount : int, message):
+    for i in range(0, amount):
+        await ctx.send(message + user1.mention)
 
 
 bot.run(TOKEN)
